497269c13739643efb1c54117a1f7dcfc57e3e19
Andrey_Del_Bello <andrey.delbello2002@gmail.com>
2022-03-08 19:07:18 +0000

    Arquivo Enviado

---
+++ Carro.java
@@ -0,0 +1,29 @@
+public class Carro implements Motor, Veiculo{
+
+    @Override
+    public String getFabricante() {
+        // TODO Auto-generated method stub
+        return "Ford";
+    }
+
+    @Override
+    public String getModelo() {
+        // TODO Auto-generated method stub
+        return "XLT";
+    }
+
+    @Override
+    public int getId() {
+        // TODO Auto-generated method stub
+        return 9696;
+    }
+
+    @Override
+    public String getNome() {
+        // TODO Auto-generated method stub
+        return "F-250";
+    }
+
+
+    
+}
---
+++ Principal.java
@@ -0,0 +1,13 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        Carro c = new Carro();
+
+        System.out.println("Meu irmão possui uma caminhonete de modelo: "+c.getModelo());
+        System.out.println("Nome do veículo: "+c.getNome());
+        System.out.println("Id do veiculo é: "+c.getId());
+        System.out.println("Fabricante do veículo: "+c.getFabricante());
+
+    }
+    
+}
